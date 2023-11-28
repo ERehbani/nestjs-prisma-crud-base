78784729116802e435376d9e7a94e58ea1cdbd5f
Elian Rehbani <elianrhbn@gmail.com>
2023-11-27 21:31:16 -0300

    nestjs prisma crud base

---
+++ src/task/task.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Put,
+  NotFoundException,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from '@prisma/client';
+
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  async getAllTask() {
+    return this.taskService.getAllTask();
+  }
+
+  @Post()
+  async createTask(@Body() data: Task) {
+    return this.taskService.createTask(data);
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: string) {
+    const taskFound = await this.taskService.getTaskById(Number(id));
+    if (!taskFound) throw new NotFoundException('Task does not exist');
+    return taskFound;
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: string) {
+    try {
+      return await this.taskService.deleteTask(Number(id));
+    } catch (error) {
+      throw new NotFoundException('Task does not exist');
+    }
+  }
+
+  @Put(':id')
+  async updateTask(@Param('id') id: string, @Body() data: Task) {
+    try {
+      return this.taskService.updateTask(Number(id), data);
+    } catch (error) {
+      throw new NotFoundException('Task does not exist');
+    }
+  }
+}
